12ad14c1c4be1b9a9004fd26716991c8ec45ad62
armanphero <017911sksaifurrahman@gmail.com>
2024-01-21 01:25:10 -0800

    page not found solve

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDwyoqDuJYj7UKRIOX8B907A3i7-Op9Fvw",
+    authDomain: "ema-john-6c4b7.firebaseapp.com",
+    projectId: "ema-john-6c4b7",
+    storageBucket: "ema-john-6c4b7.appspot.com",
+    messagingSenderId: "149555760705",
+    appId: "1:149555760705:web:cbcbec9a7fcdafe06e80c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
